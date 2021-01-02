c8cb6b77d51d8acffddb6bb068dae7a813c73a1d
DivadNojnarg <dgranjon@ymail.com>
2021-01-03 00:59:43 +0100

    test without minified files

--- R/mypkg2-dependencies.R
+++ R/mypkg2-dependencies.R
@@ -12,9 +12,9 @@ add_mypkg2_deps <- function(tag) {
   name = "mypkg2",
   version = packageVersion("mypkg2"),
   src = c(file = "mypkg2-0.0.0.9000"),
-  script = "js/mypkg2.min.js",
+  script = "js/mypkg2.js",
   package = "mypkg2",
  )
  tagList(tag, mypkg2_deps)
 }
-    
+
